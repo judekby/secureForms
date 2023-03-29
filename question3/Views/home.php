9040e3b2d69eeab1206c06ba71013c6fa33e5b69
judekabeya <judek9392@gmail.com>
2023-03-29 23:08:05 +0200

    home page

--- question3/Views/home.php
+++ question3/Views/home.php
@@ -1,4 +1,37 @@
-<?php   
-    
-$content = 'hello' ;
+<?php
+	// Initialiser la session
+	session_start();
+	// Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+	if(!isset($_SESSION["username"])){
+		header("Location: connexion.php");
+		exit(); 
+	}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Index</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+
+<header>
+	<div class="in">
+		<div class="in">
+			<div class="menu">
+				<a >Accueil</a>
+			</div>
+		</div>
+	</div>
+</header>
+
+<body>
+	<div class="text">
+		<h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
+		<p>Vous êtes maintenant connecté.</p>
+	</div>
+</body>
+</html>
